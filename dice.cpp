e1fcdd36ee888d82654b47d0ab15fcf7fb929550
Matt <mattbowen99@gmail.com>
2023-07-07 21:51:08 -0400

    Beginning layout

--- dice.cpp
+++ dice.cpp
@@ -7,18 +7,23 @@ int main(int argc, char **argv)
 	char dummy;
 
 	srand(time(NULL));
-	total = 0;
-	for(i=0; i<3;++i)
-		nums[i] = rand()%6+1;
+	while()
+	{
+		total = 0;
+		for(i=0; i<3;++i)
+			nums[i] = rand()%6+1;
 		if(argc == 1)
 		{
+
+			//print dice grahics before populating them with real numbers
 			for(i=0;i<3;i++)
 			{
-				std::cout << nums[i] << " ";
 				total+=nums[i];
 			}
-			std::cout << " = " << total << std::endl;
+			//std::cout << nums[i] << " ";
+			std::cout << "total = " << total << std::endl;
 		}
+	}
 	return 0;
 }
 
